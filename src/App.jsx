5cd89d44accf4421b1a410e5eaddd764eb237e42
hazedrops <hazedrops@hotmail.com>
2024-02-13 21:35:23 -0800

    React - useReducer - Add_Todo finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,47 @@
 import React, { useState, useReducer } from 'react'
+import Todo from './Todo.jsx'
 
-const ACTIONS = {}
+const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo'
+}
 
-function reducer(state, action) {}
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)]
+    case ACTIONS.TOGGLE_TODO:
+  }
+}
+
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false } // New todo is not completed when it's created, so false
+}
 
 export default function App() {
   const [todos, dispatch] = useReducer(reducer, [])
   const [name, setName] = useState('')
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } })
+    setName('')
+  }
+
+  console.log(todos)
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type='text'
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
       </form>
+      {todos.map(todo => {
+        return <Todo key={todo.id} todo={todo} />
+      })}
     </>
   )
 }
